57b4f06223929d49e53b4ed411b4ab64d444b196
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-02-22 09:31:31 +0100

    feat: copy routes, components, helpers + refactor from TS => JS

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,20 +1,148 @@
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import mongoose from "mongoose";
+import { PrismaClient } from "@prisma/client";
+import { Link, useLoaderData } from "@remix-run/react";
+import { format, parseISO, startOfWeek } from "date-fns";
+import EntryForm from "~/components/entry-form";
+import { getSession } from "~/session";
 
-export async function loader() {
-  const entries = await mongoose.models.Entry.find({});
-  return json({ entries });
+export async function action({ request }) {
+  let session = await getSession(request.headers.get("cookie"));
+  if (!session.data.isAdmin) {
+    throw new Response("Not authenticated", {
+      status: 401,
+      statusText: "Not authenticated",
+    });
+  }
+
+  let db = new PrismaClient();
+
+  let formData = await request.formData();
+  let { date, type, text } = Object.fromEntries(formData);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  if (
+    typeof date !== "string" ||
+    typeof type !== "string" ||
+    typeof text !== "string"
+  ) {
+    throw new Error("Bad request");
+  }
+
+  return db.entry.create({
+    data: {
+      date: new Date(date),
+      type: type,
+      text: text,
+    },
+  });
+}
+
+export async function loader({ request }) {
+  let session = await getSession(request.headers.get("cookie"));
+
+  let db = new PrismaClient();
+  let entries = await db.entry.findMany({ orderBy: { date: "desc" } });
+
+  return {
+    session: session.data,
+    entries: entries.map((entry) => ({
+      ...entry,
+      date: entry.date.toISOString().substring(0, 10),
+    })),
+  };
 }
 
 export default function Index() {
-  const { entries } = useLoaderData();
+  let { session, entries } = useLoaderData();
+
+  let entriesByWeek = entries.reduce((memo, entry) => {
+    let sunday = startOfWeek(parseISO(entry.date));
+    let sundayString = format(sunday, "yyyy-MM-dd");
+
+    memo[sundayString] ||= [];
+    memo[sundayString].push(entry);
+
+    return memo;
+  }, {});
+
+  let weeks = Object.keys(entriesByWeek).map((dateString) => ({
+    dateString,
+    work: entriesByWeek[dateString].filter((entry) => entry.type === "work"),
+    learnings: entriesByWeek[dateString].filter(
+      (entry) => entry.type === "learning",
+    ),
+    interestingThings: entriesByWeek[dateString].filter(
+      (entry) => entry.type === "interesting-thing",
+    ),
+  }));
 
   return (
-    <div className="p-8 text-slate-50 bg-slate-900">
-      <code>
-        <pre className="animate-pulse">{JSON.stringify(entries, null, 2)}</pre>
-      </code>
+    <div>
+      {session.isAdmin && (
+        <div className="mb-8 rounded-lg border border-gray-700/30 bg-gray-800/50 p-4 lg:mb-20 lg:p-6">
+          <p className="text-sm font-medium text-gray-500 lg:text-base">
+            New entry
+          </p>
+
+          <EntryForm />
+        </div>
+      )}
+
+      <div className="mt-12 space-y-12 border-l-2 border-sky-500/[.15] pl-5 lg:space-y-20 lg:pl-8">
+        {weeks.map((week) => (
+          <div key={week.dateString} className="relative">
+            <div className="absolute left-[-34px] rounded-full bg-gray-900 p-2 lg:left-[-46px]">
+              <div className="h-[10px] w-[10px] rounded-full border border-sky-500 bg-gray-900" />
+            </div>
+
+            <p className="pt-[5px] text-xs font-semibold uppercase tracking-wider text-sky-500 lg:pt-[3px] lg:text-sm">
+              Week of {format(parseISO(week.dateString), "MMMM d, yyyy")}
+            </p>
+
+            <div className="mt-6 space-y-8 lg:space-y-12">
+              <EntryList entries={week.work} label="Work" />
+              <EntryList entries={week.learnings} label="Learnings" />
+              <EntryList
+                entries={week.interestingThings}
+                label="Interesting things"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
+  );
+}
+
+function EntryList({ entries, label }) {
+  return entries.length > 0 ? (
+    <div>
+      <p className="font-semibold text-white">{label}</p>
+
+      <ul className="mt-4 space-y-6">
+        {entries.map((entry) => (
+          <EntryListItem key={entry.id} entry={entry} />
+        ))}
+      </ul>
+    </div>
+  ) : null;
+}
+
+function EntryListItem({ entry }) {
+  let { session } = useLoaderData();
+
+  return (
+    <li className="group leading-7">
+      {entry.text}
+
+      {session.isAdmin && (
+        <Link
+          to={`/entries/${entry.id}/edit`}
+          className="ml-2 text-sky-500 opacity-0 group-hover:opacity-100"
+        >
+          Edit
+        </Link>
+      )}
+    </li>
   );
 }
