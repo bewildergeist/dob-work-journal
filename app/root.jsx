42745c38ffae1bfd9bd4349523afc48db80300f6
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-02-22 23:39:46 +0100

    12: Adding a logout button
    https://buildui.com/courses/ship-an-app-with-remix/adding-a-logout-button

--- app/root.jsx
+++ app/root.jsx
@@ -1,13 +1,18 @@
+import { redirect } from "@remix-run/node";
 import {
+  Form,
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 import stylesheet from "~/tailwind.css";
+import { destroySession, getSession } from "./session.server";
 
 export function links() {
   return [{ rel: "stylesheet", href: stylesheet }];
@@ -17,7 +22,28 @@ export function meta() {
   return [{ title: "Work Journal" }];
 }
 
+/* ACTION --------------------------------------------------------- */
+export async function action({ request }) {
+  let session = await getSession(request.headers.get("cookie"));
+
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+}
+
+/* LOADER --------------------------------------------------------- */
+export async function loader({ request }) {
+  let session = await getSession(request.headers.get("cookie"));
+
+  return { session: session.data };
+}
+
+/* UI ------------------------------------------------------------- */
 export default function App() {
+  const { session } = useLoaderData();
+
   return (
     <html lang="en">
       <head>
@@ -28,10 +54,22 @@ export default function App() {
       </head>
       <body>
         <div className="p-10">
-          <h1 className="text-5xl">Work Journal</h1>
-          <p className="mt-2 text-lg text-gray-400">
-            Learnings and doings. Updated weekly.
-          </p>
+          <div className="flex items-start justify-between">
+            <div>
+              <h1 className="text-5xl">Work Journal</h1>
+              <p className="mt-2 text-lg text-gray-400">
+                Learnings and doings. Updated weekly.
+              </p>
+            </div>
+
+            {session.isAdmin ? (
+              <Form method="post">
+                <button>Logout</button>
+              </Form>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+          </div>
 
           <Outlet />
         </div>
