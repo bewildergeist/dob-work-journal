8c0523ddd96be422882d59ed323c49fadec10e14
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-02-22 21:03:24 +0100

    04: Adding pending UI
    https://buildui.com/courses/ship-an-app-with-remix/adding-pending-ui

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,23 +1,39 @@
-import { redirect } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { useEffect, useRef } from "react";
+import { useFetcher } from "@remix-run/react";
+import { format } from "date-fns";
 import mongoose from "mongoose";
 
 export async function action({ request }) {
   let formData = await request.formData();
 
+  // Artificially slow down the form submission to show pending UI
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   // Mongoose will throw an error if the entry data is invalid (and we don't
   // catch it here, so it will be caught by Remix's error boundary and displayed
   // to the user — we can improve on that later)
-  await mongoose.models.Entry.create({
+  return mongoose.models.Entry.create({
     date: new Date(formData.get("date")),
     type: formData.get("type"),
     text: formData.get("text"),
   });
-
-  return redirect("/");
 }
 
 export default function Index() {
+  const fetcher = useFetcher();
+  const textareaRef = useRef(null);
+
+  const isIdle = fetcher.state === "idle";
+  const isInit = isIdle && fetcher.data == null;
+
+  // Clear the form and focus the textarea after a submission
+  useEffect(() => {
+    if (!isInit && isIdle && textareaRef.current) {
+      textareaRef.current.value = "";
+      textareaRef.current.focus();
+    }
+  }, [isInit, isIdle]);
+
   return (
     <div className="p-10">
       <h1 className="text-5xl">Work Journal</h1>
@@ -28,53 +44,72 @@ export default function Index() {
       <div className="my-8 border p-3">
         <p className="italic">Create a new entry</p>
 
-        <Form method="post" className="mt-2">
-          <div>
+        <fetcher.Form method="post" className="mt-2">
+          <fieldset
+            className="disabled:opacity-70"
+            disabled={fetcher.state === "submitting"}
+          >
             <div>
-              <input type="date" name="date" id="" className="text-gray-500" />
-            </div>
-            <div className="mt-4 space-x-4">
-              <label className="inline-block">
-                <input type="radio" className="mr-1" name="type" value="work" />
-                Work
-              </label>
-              <label className="inline-block">
+              <div>
                 <input
-                  type="radio"
-                  className="mr-1"
-                  name="type"
-                  value="learning"
+                  type="date"
+                  name="date"
+                  required
+                  className="text-gray-900"
+                  defaultValue={format(new Date(), "yyyy-MM-dd")}
                 />
-                Learning
-              </label>
-              <label className="inline-block">
-                <input
-                  type="radio"
-                  className="mr-1"
-                  name="type"
-                  value="interesting-thing"
-                />
-                Interesting thing
-              </label>
+              </div>
+              <div className="mt-4 space-x-4">
+                <label className="inline-block">
+                  <input
+                    required
+                    type="radio"
+                    defaultChecked
+                    className="mr-1"
+                    name="type"
+                    value="work"
+                  />
+                  Work
+                </label>
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="learning"
+                  />
+                  Learning
+                </label>
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="interesting-thing"
+                  />
+                  Interesting thing
+                </label>
+              </div>
             </div>
-          </div>
-          <div className="mt-4">
-            <textarea
-              placeholder="Type your entry..."
-              name="text"
-              id=""
-              className="w-full text-gray-700"
-            />
-          </div>
-          <div className="mt-2 text-right">
-            <button
-              type="submit"
-              className="bg-blue-500 px-4 py-1 font-semibold text-white"
-            >
-              Save
-            </button>
-          </div>
-        </Form>
+            <div className="mt-4">
+              <textarea
+                ref={textareaRef}
+                placeholder="Type your entry..."
+                name="text"
+                className="w-full text-gray-700"
+                required
+              />
+            </div>
+            <div className="mt-2 text-right">
+              <button
+                type="submit"
+                className="bg-blue-500 px-4 py-1 font-semibold text-white"
+              >
+                {fetcher.state === "submitting" ? "Saving..." : "Save"}
+              </button>
+            </div>
+          </fieldset>
+        </fetcher.Form>
       </div>
 
       <div className="mt-6">
