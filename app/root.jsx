3eeca393bbb34bc80153b054d541e2bcd8bcfbe7
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-02-22 09:46:28 +0100

    fix: update meta function to export array

--- app/root.jsx
+++ app/root.jsx
@@ -21,11 +21,13 @@ export const links = () => [
   { rel: "stylesheet", href: "/fonts/inter/inter.css" },
 ];
 
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta = () => [
+  {
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
+  },
+];
 
 export async function action({ request }) {
   let session = await getSession(request.headers.get("cookie"));
