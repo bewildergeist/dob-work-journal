f7cb0a3efb8ccffe4a8693c58286b6fe740b86ad
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-02-25 22:42:33 +0100

    19: Deploying our app
    https://buildui.com/courses/ship-an-app-with-remix/deploying-our-app

--- app/components/entry-form.jsx
+++ app/components/entry-form.jsx
@@ -71,7 +71,7 @@ export default function EntryForm({ entry }) {
             type="submit"
             className="w-full rounded-md bg-sky-600 px-4 py-2 text-sm font-medium text-white hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:ring-offset-2 focus:ring-offset-gray-900 lg:w-auto lg:py-1.5"
           >
-            {fetcher.state !== "idle" ? "Saving..." : "Save"}{" "}
+            {fetcher.state !== "idle" ? "Saving..." : "Save"}
           </button>
         </div>
       </fieldset>
